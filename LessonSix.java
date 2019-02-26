6ed218cf0e7e8ed559712ea33f628f84892a9693
juliankanaan <46693750+juliankanaan@users.noreply.github.com>
2019-02-26 18:46:15 -0500

    Add files via upload

---
+++ LessonSix.java
@@ -0,0 +1,29 @@
+// Methods pt 2
+
+public class LessonSix {
+
+
+  /*
+  Aight so you can create two methods w/ same name
+  as long as they accept different params.
+
+  [Why the hell would you do such a thing, you monsters]
+
+  Ah, maybe a findMax function that accepts a varying num of params or diff data types
+
+  Compiler will try to find the best match for the method you call
+
+  */
+
+  /*
+  Variables defined within each method are local.
+
+  IMPORTANT: variables defined outside of a loop (even w/in the same method),
+  aren't immedicately recognized. must be re-defined.
+  */
+
+  public static void main(String[] args) {
+
+  }
+
+}
