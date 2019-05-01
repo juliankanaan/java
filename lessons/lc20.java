a619341e1fca6aff254fe2e81daf6d70c3f99f71
Julian Kanaan <kanaan@navocapital.com>
2019-05-01 11:01:47 -0400

    Create lc20.java

---
+++ lessons/lc20.java
@@ -0,0 +1,27 @@
+import java.util.*;
+/*
+Can use an abstract class as a data type
+
+
+interfaces are like abstract classes: can't have a body
+
+public interface InterfaceNAme {
+  constant declaration1; # part 1
+
+  // abstract methods...
+
+}
+// kind of like extends
+
+class MyClass implements InterfaceName{
+
+}
+// check if n ibject implements an Interface
+  if (object instanceof Interface){
+    // object implemented the interface
+  }
+
+sort objects?
+  make your class implement the Comparable interface
+  this only has one method: compareTo(obj)
+*/
